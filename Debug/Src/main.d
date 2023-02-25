0d51335818a98fd622bb49fd04616f7b750f2f92
Mike1702 <mai.ptx203734@sis.hust.edu.vn>
2023-02-25 16:11:52 +0700

    content

---
+++ Debug/Src/main.d
@@ -0,0 +1,19 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/Admin/STM32CubeIDE/workspace_1.11.0/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_gpio.h \
+ C:/Users/Admin/STM32CubeIDE/workspace_1.11.0/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h \
+ C:/Users/Admin/STM32CubeIDE/workspace_1.11.0/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/Admin/STM32CubeIDE/workspace_1.11.0/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/Admin/STM32CubeIDE/workspace_1.11.0/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/Admin/STM32CubeIDE/workspace_1.11.0/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h \
+ C:/Users/Admin/STM32CubeIDE/workspace_1.11.0/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h \
+ C:/Users/Admin/STM32CubeIDE/workspace_1.11.0/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/led/led.h \
+ C:/Users/Admin/STM32CubeIDE/workspace_1.11.0/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/button/button.h
+C:/Users/Admin/STM32CubeIDE/workspace_1.11.0/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_gpio.h:
+C:/Users/Admin/STM32CubeIDE/workspace_1.11.0/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h:
+C:/Users/Admin/STM32CubeIDE/workspace_1.11.0/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h:
+C:/Users/Admin/STM32CubeIDE/workspace_1.11.0/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h:
+C:/Users/Admin/STM32CubeIDE/workspace_1.11.0/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h:
+C:/Users/Admin/STM32CubeIDE/workspace_1.11.0/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h:
+C:/Users/Admin/STM32CubeIDE/workspace_1.11.0/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h:
+C:/Users/Admin/STM32CubeIDE/workspace_1.11.0/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/led/led.h:
+C:/Users/Admin/STM32CubeIDE/workspace_1.11.0/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/button/button.h:
